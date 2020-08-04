6e235c34e8a5a47761b844967d2c39bd9a789a3d
kkalla <dkswns333@gmail.com>
2020-08-04 21:28:18 +0900

    misc

---
+++ web_dev/demo/model.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+
+def show_color(username):
+    connection = sqlite3.connect('flask_tut.db', check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute(
+        """
+        SELECT favorite_color FROM users WHERE username='{username}'
+        ORDER BY pk DESC;
+        """.format(username=username)
+    )
+    color = cursor.fetchone()[0]
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+    message = "'{username}'s favorite color is '{color}'.".format(
+        username=username, color=color)
+    return message
---
+++ web_dev/demo/seed.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+connection = sqlite3.connect("flask_tut.db", check_same_thread=False)
+cursor = connection.cursor()
+
+cursor.execute(
+    """INSERT INTO users(
+        username,
+        password,
+        favorite_color
+    )VALUES(
+        'Gordon',
+        'Ramsay',
+        'Red'
+    );"""
+)
+
+cursor.execute(
+    """INSERT INTO users(
+        username,
+        password,
+        favorite_color
+    )VALUES(
+        'Ironman',
+        'Tony',
+        'Blue'
+    );"""
+)
+
+connection.commit()
+cursor.close()
+connection.close()
