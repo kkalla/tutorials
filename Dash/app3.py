5fd0849a24058d6a652fdda296b64dd1db241cb3
kkalla <dkswns333@gmail.com>
2020-08-10 18:13:20 +0900

    NAS update docker-compose

--- Dash/app3.py
+++ Dash/app3.py
@@ -105,12 +105,63 @@ app.layout = html.Div([
 
     # Update Graphs on Hover
     html.Div([
+
         html.Div([
             dcc.Dropdown(
                 id='crossfilter-xaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Fertility rate, total (births per woman)'
+            ),
+            dcc.RadioItems(
+                id='crossfilter-xaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
             )
-        ])
-    ])
+        ],
+        style={'width': '49%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Dropdown(
+                id='crossfilter-yaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Life expectancy at birth, total (years)'
+            ),
+            dcc.RadioItems(
+                id='crossfilter-yaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )
+        ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
+    ], style={
+        'borderBottom': 'thin lightgrey solid',
+        'backgroundColor': 'rgb(250, 250, 250)',
+        'padding': '10px 5px'
+    }),
+
+    html.Div([
+        dcc.Graph(
+            id='crossfilter-indicator-scatter',
+            hoverData={'points': [{'customdata': 'Japan'}]}
+        )
+    ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20 '}),
+
+    html.Div([
+        dcc.Graph(id='x-time-series'),
+        dcc.Graph(id='y-time-series')
+    ], style={'display': 'inline-block', 'width': '49%'}),
+
+    html.Div(
+        dcc.Slider(
+            id='crossfilter-year--slider',
+            min=df['Year'].min(),
+            max=df['Year'].max(),
+            value=df['Year'].max(),
+            marks={str(year): str(year) for year in df['Year'].unique()},
+            step=None
+        ), style={'width': '49%', 'padding': '0px 20px 20px 20px'}
+    )
 ])
 
 
@@ -140,6 +191,51 @@ def display_selected_data(selectedData):
     [Input('basic-interactions', 'relayoutData')])
 def display_relayout_data(relayoutData):
     return json.dumps(relayoutData, indent=2)
+
+
+@app.callback(
+    Output('crossfilter-indicator-scatter', 'figure'),
+    [Input('crossfilter-xaxis-column', 'value'),
+     Input('crossfilter-xaxis-type', 'value'),
+     Input('crossfilter-yaxis-column', 'value'),
+     Input('crossfilter-yaxis-type', 'value'),
+     Input('crossfilter-year--slider', 'value'),])
+def update_graph(x_col_name, y_col_name, xaxis_type, yaxis_type, year_value):
+    dff = df[df['Year'] == year_value]
+
+    return {
+        'data': [dict(
+            x=dff[dff['Indicator Name'] == x_col_name]['Value'],
+            y=dff[dff['Indicator Name'] == y_col_name]['Value'],
+            text=dff[dff['Indicator Name'] == y_col_name]['Country Name'],
+            customdata=dff[dff['Indicator Name'] == y_col_name]['Country Name'],
+            mode='markers',
+            marker={
+                'size': 15,
+                'opacity': 0.5,
+                'line': {'width': 0.5, 'color': 'white'}
+            }
+        )],
+        'layout': dict(
+            xaxis={
+                'title': x_col_name,
+                'type': 'linear' if xaxis_type == 'Linear' else 'log'
+            },
+            yaxis={
+                'title': y_col_name,
+                'type': 'linear' if yaxis_type == 'Linear' else 'log'
+            },
+            margin={'l': 40, 'b': 30, 't': 10, 'r': 0},
+            height=450,
+            hovermode='closest'
+        )
+    }
+
+
+def create_time_series(dff, axis_type, title):
+    return {
+        
+    }
 
 
 if __name__ == '__main__':
--- nas/README.md
+++ nas/README.md
@@ -5,3 +5,4 @@
 - https://blog.wise-leader.com/01-sambaserver/
 - https://linuxconfig.org/how-to-create-a-docker-based-lamp-stack-using-docker-on-ubuntu-20-04
 - https://www.smarthomebeginner.com/docker-home-media-server-2018-basic/
+- [nextcloud dockerhub](https://hub.docker.com/_/nextcloud/)
